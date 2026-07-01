f8b2938eb693e0153b9b5833f8fd9bf3b5803f5f
Kiraxarik <adamchrisdraper@gmail.com>
2026-07-01 08:19:10 -0500

    doodoo

--- Assets/Server/Player/GoInGameRequest.cs
+++
@@ -1,8 +0,0 @@
-using Unity.NetCode;
-
-/// <summary>
-/// Sent by the client to the server once the connection is established,
-/// meaning "I'm ready — please spawn my player and start sending me
-/// snapshots." Contains no data; just acts as a signal.
-/// </summary>
-public struct GoInGameRequest : IRpcCommand { }
